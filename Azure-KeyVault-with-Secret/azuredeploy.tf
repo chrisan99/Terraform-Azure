c5f939cb7db50686afc42c79c9a968f80c3bc9dd
chrisan99 <40714470+chrisan99@users.noreply.github.com>
2021-12-11 21:03:57 +0800

    Update azuredeploy.tf

--- Azure-KeyVault-with-Secret/azuredeploy.tf
+++ Azure-KeyVault-with-Secret/azuredeploy.tf
@@ -29,6 +29,7 @@ resource "azurerm_key_vault" "kv1" {
   resource_group_name         = var.azure-rg-1
   enabled_for_disk_encryption = true
   enabled_for_template_deployment = true
+  enabled_for_deployment      = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
